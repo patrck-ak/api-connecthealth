1be53ef660ebf11ecb240492b24b89d73f1e4b49
Patrick <patrickoliveira.silva20@gmail.com>
2023-12-21 22:34:18 -0300

    sistema de rotas privadas - 0.2.0

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 // importação do .env
 require("dotenv").config();
 
@@ -11,12 +10,13 @@ const { access } = require("fs");
 const User = require("./models/User");
 var cors = require("cors");
 
+const secret = '9DS0AD7ADA7DY028DASUDAS09DA8D9A8'
+
 const app = express();
 const port = 5000;
 const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true,
-  optionSuccessStatus: 200,
 };
 
 // config express
@@ -24,46 +24,101 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors(corsOptions));
 
-//* requrest da página de login
+//* logar usuário
 app.post("/auth/user", async (req, res, next) => {
   const { name, pass } = req.body;
 
   if (!name) {
-    return res.json({erroMsg: "ID em branco ou inválida." });
-  } if(!pass) {
-    return res.json({erroMsg: "Senha em branco ou inválida."})
+    return res.json({ err: "ID em branco ou inválida.", status: 1 })
+  }
+  if (!pass) {
+    return res.json({ err: "Senha em branco ou inválida.", status: 2  });
   }
 
   //* busca o usuario no banco
-  const user = await User.findOne({name: name})
+  const user = await User.findOne({ name: name });
   //! caso não exista retorna json de erro
-  if(!user) { 
-    return res.json({erroMsg: 'Usuário não encontrado.'})
+  if (!user) {
+    return res.json({ err: "Usuário não encontrado.", status: 3  });
   }
 
   //* compara o input de senha com o hash do banco
-  const checkPassword = await bcrypt.compare(pass, user.password)
-  if(!checkPassword) { 
-    return res.json({erroMsg: 'Senha incorreta.'})
+  const checkPassword = await bcrypt.compare(pass, user.password);
+  console.log(checkPassword, pass);
+  if (!checkPassword) {
+    return res.json({ err: "Senha incorreta.", status: 4  });
+  } else {
+    const secret = process.env.SECRET
+    const token = jwt.sign({id: user._id}, secret)
+    return res.send({ name: user.name, status: 5, id: user.id, token: token});
   }
-  res.json({msg: 'logado.'})
 });
 
 //* cadastrar usuario admin
-app.post("/new/user/admin", (req, res) => {
-  // recupera todos os inputs 
-  const {name, pass, level} = req.body
-  if(!name) {
-    return res.json({erroMsg: "UserID em branco ou inválido."})
+app.post("/user/new/admin", async (req, res) => {
+  // recupera todos os inputs
+  const { name, pass, email, level } = req.body;
+  if (!name) {
+    return res.json({ err: "UserID em branco ou inválido." });
   }
-  if(!pass) {
-    return res.json({erroMsg: "UserID em branco ou inválido."})
+  if (!pass) {
+    return res.json({ err: "Senha em branco ou inválido." });
   }
-  if(!level) {
-    return res.json({erroMsg: "Nivel de permissão em branco ou inválido."})
+  if (!email) {
+    return res.json({ err: "E-mail em branco ou inválido." });
   }
-})
+  if (!level) {
+    return res.json({ err: "Nivel de permissão em branco ou inválido." });
+  }
 
+  //* verifica se usuario já existe
+  const userExists = await User.findOne({ name: name });
+  const mailExists = await User.findOne({ email: email });
+
+  if (userExists) {
+    return res.json({ err: "Usuário já cadastrado" });
+  }
+  if (mailExists) {
+    return res.json({ msg: "E-mail já cadastrado" });
+  }
+
+  //* criptar senha DB
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hash(pass, salt);
+
+  //* salvar usuário na tabela
+  const user = new User({
+    name,
+    email,
+    password: passwordHash,
+    level,
+  });
+
+  //* cria um usuário
+  try {
+    await user.save();
+    res.status(201).json({ msg: "usuario criado com sucesso." });
+  } catch (error) {
+    // retorna erro caso tenha algum
+    console.log(error);
+  }
+});
+
+app.post("/validation", async (req, res) => {
+  const { tk, id } = req.body;
+  var tokenValid = false
+  try { //* logger de tentavida de token inválido.
+    tokenValid = jwt.verify(tk, secret)
+  } catch (error) {
+    console.error('token inválido: ' + tk)
+  }
+  const userExists = User.findOne({_id: id})
+  if(tokenValid && userExists) {
+    res.json({stts: true})
+  } else {
+    res.json({stts: false})  
+  }
+});
 
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
