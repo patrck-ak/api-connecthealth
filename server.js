f9eea2a3007e122cfbbddcf52096cefe935c66ed
Patrick <patrickoliveira.silva20@gmail.com>
2023-12-23 17:35:59 -0300

    configuração cors - 1.1

--- server.js
+++ server.js
@@ -24,6 +24,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors(corsOptions));
 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
+
+
 app.get('/', async (req, res) => {
   res.json({msg: 'Olá :)'})
 })
